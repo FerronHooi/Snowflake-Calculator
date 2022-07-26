c3a2f9beded166fa4aaec336220a760d8f0e49a1
Ferron Hooi <102597092+FerronHooiInergy@users.noreply.github.com>
2022-07-26 12:05:38 +0200

    uploaded files

---
+++ Backup files/list to iterate.js
@@ -0,0 +1,40 @@
+const priceListCategoryIds = ['priceUSDbusinessCritical', 'priceEURbusinessCritical', 'priceGBPbusinessCritical', 'priceUSDenterprise', 'priceEURenterprise', 'priceUSDstandard', 'priceEURstandard']
+const tagIds = ['business-critical', 'enterprise', 'standard', 'virtual-private-snowflake-vps']
+const awsPlatformIds = ['amazonwebservicesaws-useastohio',
+'amazonwebservicesaws-useastohio',
+'amazonwebservicesaws-uswestoregon',
+'amazonwebservicesaws-canadacentral', 
+'amazonwebservicesaws-eulondon',
+'amazonwebservicesaws-euireland',
+'amazonwebservicesaws-eufrankfurt',
+'amazonwebservicesaws-europestockholm',
+'amazonwebservicesaws-asiapacificsydney',
+'amazonwebservicesaws-asiapacificsingapore',
+'amazonwebservicesaws-asiapacifictokyo',
+'amazonwebservicesaws-asiapacificosaka',
+'amazonwebservicesaws-asiapacificseoul',
+'amazonwebservicesaws-asiapacificmumbai',
+'amazonwebservicesaws-useastcommercialgovnva',
+'amazonwebservicesaws-southamericaeast1saopaulo']
+
+const azurePlatformIds = ['microsoftazure-eastus2virginia',
+'microsoftazure-westus2washington',
+'microsoftazure-centralusiowa',
+'microsoftazure-southcentralustexas',
+'microsoftazure-canadacentraltoronto',
+'microsoftazure-westeuropenetherlands',
+'microsoftazure-northeuropeireland', 
+'microsoftazure-australiaeastsydney', 
+'microsoftazure-southeastasiasingapore', 
+'microsoftazure-switzerlandnorthzurich', 
+'microsoftazure-usgovvirginia', 
+'microsoftazure-japaneasttokyo', 
+'microsoftazure-uaenorthdubai', 
+'microsoftazure-centralindiapune']
+
+const googleCloudPlatformIds = ['googlecloudplatform-uscentral1iowa', 
+'googlecloudplatform-useast4nvirginia',
+'googlecloudplatform-europewest2london',
+'googlecloudplatform-europewest4netherlands']
+
+console.log(awsPlatformIds, azurePlatformIds, googleCloudPlatformIds)
