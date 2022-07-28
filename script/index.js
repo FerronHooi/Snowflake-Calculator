d2f1a0020802f2aaf1af3cccecec2c7907c2e463
Ferron Hooi <102597092+FerronHooiInergy@users.noreply.github.com>
2022-07-28 10:18:21 +0200

    all data now found and scraped (but no scraper made yet..)

--- script/index.js
+++ script/index.js
@@ -95,3 +95,43 @@ axios(url)
 
 app.listen(PORT, () => console.log('server running on port' + PORT))
 
+const PORT = 8000
+const axios = require('axios')
+const cheerio = require('cheerio')
+const express = require('express')
+
+const app = express()
+
+//definieeren van op te halen URL 
+const url = 'https://www.snowflake.com/pricing/'
+
+axios(url)
+    .then(response =>{
+        const html = response.data
+        const $ = cheerio.load(html)
+        const price = []
+        //PRIJZEN KOMEN NOG NIET OVEREEN MET DE JUISTE REGIO BLIJKBAAR....
+
+                //Microsoft Azure (gebruikt wildcard, hij pakt dus alles met microsoftazure....)
+                $('[id^=microsoftazure]', html).each(function(){
+
+                const cloudregion = $(this).attr('id')
+                const dataStandard = { tier: $('#standard').attr('id'), priceUSD: $('#standard').find('div.pricing-price').attr('data-price-usd'), priceEUR: $('#standard').find('div.pricing-price').attr('data-price-eur'), priceGBP: $('#standard').find('div.pricing-price').attr('data-price-gbp') }
+                const dataEnterprise = { tier: $('#enterprise').attr('id'), priceUSD: $('#enterprise').find('div.pricing-price').attr('data-price-usd'), priceEUR: $('#enterprise').find('div.pricing-price').attr('data-price-eur'), priceGBP: $('#enterprise').find('div.pricing-price').attr('data-price-gbp') }
+                const dataBusinessCritical = { tier: $('#business-critical').attr('id'), priceUSD: $('#business-critical').find('div.pricing-price').attr('data-price-usd'), priceEUR: $('#business-critical').find('div.pricing-price').attr('data-price-eur'), priceGBP: $('#business-critical').find('div.pricing-price').attr('data-price-gbp') }
+
+                        
+                price.push({
+                    
+                    cloudregion,
+                    dataStandard,
+                    dataEnterprise,
+                    dataBusinessCritical
+                        
+                })
+                
+            })
+
+                }).catch(err => console.log(err))
+
+app.listen(PORT, () => console.log('server running on port' + PORT))
