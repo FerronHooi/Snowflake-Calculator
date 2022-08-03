300d6421b2550cc45e1768df453bd9dae2a04ebd
Ferron Hooi <102597092+FerronHooiInergy@users.noreply.github.com>
2022-08-03 12:35:19 +0200

    edited some things to make the code clearer

--- script/index.js
+++ script/index.js
@@ -4,41 +4,35 @@ const cheerio = require('cheerio')
 //definieeren van op te halen URL 
 const url = 'https://www.snowflake.com/pricing/'
 
+const scrapeDataFromSnowflakePricing = async () => {
     axios(url)
-    .then(response =>{
-        const html = response.data
-        const $ = cheerio.load(html)
-        const script = []
+        .then(response => {
+            const html = response.data
+            const $ = cheerio.load(html)
 
-                $('[id=content]', html).each(function(){
+            $('[id=content]', html).each(function () {
 
                 //scraper voor de aanwezige script. de .replace verwijdert de /n en '    
-                const data = $(this).find('script').text().replace(/\n/g,'')
+                const data = $(this).find('script').text().replace(/\n/g, '')
                 //de text bevat een stuk tekst ervoor en erna die verwijdert moet worden. Dat gebeurd hier
-                const subStringData = data.substring(
-                    data.indexOf("($)") + 8, 
+                const script = data.substring(
+                    data.indexOf("($)") + 8,
                     data.indexOf("// custom select ")
                 );
+                
+                console.log(script)
 
                 //write string to javascript file
-                /*var fs = require('fs');
-                fs.writeFile ("snowflakeCloudDataScript.js", subStringData, function(err) {
+                var fs = require('fs');
+                fs.writeFile ("snowflakeCloudDataScript.js", script, function(err) {
                     if (err) throw err;
                     console.log('complete');
-                    })*/
- 
-                script.push({
-                    
-                    subStringData
-                        
-                })
+                })            
                                 
             })
-                                
-        console.log(script)
 
-    }).catch(err => console.log(err))  
+        }
+        ).catch(err => console.log(err))
+}
 
-//TODO: IF SCRAPER RETURNS ERROR, DO NOT OVERWRITE snowflakeCloudDataScript.js
-
-
+scrapeDataFromSnowflakePricing()
