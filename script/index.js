356b52b88a9b1d436b6e9359ad00e410d1904df5
Ferron Hooi <102597092+FerronHooiInergy@users.noreply.github.com>
2022-08-09 10:51:05 +0200

    added old scraper back to the code...

--- script/index.js
+++ script/index.js
@@ -12,6 +12,7 @@ const scrapeDataFromSnowflakePricing = async () => {
 
             $('[id=content]', html).each(function () {
 
+                //ON DEMAND STORAGE EN CAPACITY STORAGE
                 //scraper voor de aanwezige script. de .replace verwijdert de /n en '    
                 const data = $(this).find('script').text().replace(/\n/g, '')
                 //de text bevat een stuk tekst ervoor en erna die verwijdert moet worden. Dat gebeurd hier
@@ -22,12 +23,162 @@ const scrapeDataFromSnowflakePricing = async () => {
                 
                 console.log(script)
 
-                //write string to javascript file
-                var fs = require('fs');
-                fs.writeFile ("snowflakeCloudDataScript.js", script, function(err) {
-                    if (err) throw err;
-                    console.log('complete');
-                })            
+                // //write to javascript file
+                // var fs = require('fs');
+                // fs.writeFile ("snowflakeCloudDataScript.js", script, function(err) {
+                //     if (err) throw err;
+                //     console.log('complete');
+                // })   
+             
+            //PRIJZEN PER REGIO IN EUR, GBP OF USD    
+            const price = [];
+            //TODO: PRIJZEN KOMEN NOG NIET OVEREEN MET DE JUISTE REGIO BLIJKBAAR....
+
+            //Microsoft Azure (gebruikt wildcard, hij pakt dus alles met microsoftazure....)
+            $('[id^=microsoftazure]', html).each(function () {
+            const cloudregion = $(this).attr('id');
+            const dataStandard = {
+                tier: $('#standard').attr('id'),
+                priceUSD: $('#standard')
+                .find('div.pricing-price')
+                .attr('data-price-usd'),
+                priceEUR: $('#standard')
+                .find('div.pricing-price')
+                .attr('data-price-eur'),
+                priceGBP: $('#standard')
+                .find('div.pricing-price')
+                .attr('data-price-gbp'),
+            };
+            const dataEnterprise = {
+                tier: $('#enterprise').attr('id'),
+                priceUSD: $('#enterprise')
+                .find('div.pricing-price')
+                .attr('data-price-usd'),
+                priceEUR: $('#enterprise')
+                .find('div.pricing-price')
+                .attr('data-price-eur'),
+                priceGBP: $('#enterprise')
+                .find('div.pricing-price')
+                .attr('data-price-gbp'),
+            };
+            const dataBusinessCritical = {
+                tier: $('#business-critical').attr('id'),
+                priceUSD: $('#business-critical')
+                .find('div.pricing-price')
+                .attr('data-price-usd'),
+                priceEUR: $('#business-critical')
+                .find('div.pricing-price')
+                .attr('data-price-eur'),
+                priceGBP: $('#business-critical')
+                .find('div.pricing-price')
+                .attr('data-price-gbp'),
+            };
+
+            price.push({
+                cloudregion,
+                dataStandard,
+                dataEnterprise,
+                dataBusinessCritical,
+            });
+            });
+
+            //Amazon Web Service AWS
+            $('[id^=amazonwebservicesaws]', html).each(function () {
+            const cloudregion = $(this).attr('id');
+            const dataStandard = {
+                tier: $('#standard').attr('id'),
+                priceUSD: $('#standard')
+                .find('div.pricing-price')
+                .attr('data-price-usd'),
+                priceEUR: $('#standard')
+                .find('div.pricing-price')
+                .attr('data-price-eur'),
+                priceGBP: $('#standard')
+                .find('div.pricing-price')
+                .attr('data-price-gbp'),
+            };
+            const dataEnterprise = {
+                tier: $('#enterprise').attr('id'),
+                priceUSD: $('#enterprise')
+                .find('div.pricing-price')
+                .attr('data-price-usd'),
+                priceEUR: $('#enterprise')
+                .find('div.pricing-price')
+                .attr('data-price-eur'),
+                priceGBP: $('#enterprise')
+                .find('div.pricing-price')
+                .attr('data-price-gbp'),
+            };
+            const dataBusinessCritical = {
+                tier: $('#business-critical').attr('id'),
+                priceUSD: $('#business-critical')
+                .find('div.pricing-price')
+                .attr('data-price-usd'),
+                priceEUR: $('#business-critical')
+                .find('div.pricing-price')
+                .attr('data-price-eur'),
+                priceGBP: $('#business-critical')
+                .find('div.pricing-price')
+                .attr('data-price-gbp'),
+            };
+
+            price.push({
+                cloudregion,
+                dataStandard,
+                dataEnterprise,
+                dataBusinessCritical,
+            });
+            });
+
+            //Google Cloud Platform
+            $('[id^=googlecloudplatform]', html).each(function () {
+            const cloudregion = $(this).attr('id');
+            const dataStandard = {
+                tier: $('#standard').attr('id'),
+                priceUSD: $('#standard')
+                .find('div.pricing-price')
+                .attr('data-price-usd'),
+                priceEUR: $('#standard')
+                .find('div.pricing-price')
+                .attr('data-price-eur'),
+                priceGBP: $('#standard')
+                .find('div.pricing-price')
+                .attr('data-price-gbp'),
+            };
+            const dataEnterprise = {
+                tier: $('#enterprise').attr('id'),
+                priceUSD: $('#enterprise')
+                .find('div.pricing-price')
+                .attr('data-price-usd'),
+                priceEUR: $('#enterprise')
+                .find('div.pricing-price')
+                .attr('data-price-eur'),
+                priceGBP: $('#enterprise')
+                .find('div.pricing-price')
+                .attr('data-price-gbp'),
+            };
+            const dataBusinessCritical = {
+                tier: $('#business-critical').attr('id'),
+                priceUSD: $('#business-critical')
+                .find('div.pricing-price')
+                .attr('data-price-usd'),
+                priceEUR: $('#business-critical')
+                .find('div.pricing-price')
+                .attr('data-price-eur'),
+                priceGBP: $('#business-critical')
+                .find('div.pricing-price')
+                .attr('data-price-gbp'),
+            };
+
+            price.push({
+                cloudregion,
+                dataStandard,
+                dataEnterprise,
+                dataBusinessCritical,
+            });
+            });
+            
+            console.log(price)    
                                 
             })
 
